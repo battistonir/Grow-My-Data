9775c9e6b59d27166875a0afb8f8850cc1aaca7e
jchan02 <jchan02@manhattan.edu>
2021-04-22 10:48:40 -0400

    Four VH400 test

--- GMDProbeSensorTest.ino
+++ GMDProbeSensorTest.ino
@@ -8,7 +8,7 @@
 #define LOG_INTERVAL 10000 //milliseconds between entries (6 minutes = 360000)
 
 //Pin assignments
-const int soilMoisturePin = A1;
+const int soilMoisturePin[5] = {A1, A2, A3, A4};
 const int dhtPin = 2;
 const int sunlightPin = A0;
 
@@ -17,7 +17,7 @@ RTC_DS1307 rtc;
 
 //Initialize variables
 float soilMoistureRaw = 0; //Raw analog input of soil moisture sensor (volts)
-float soilMoisture = 0;    //Scaled value of volumetric water content in soil (percent)
+float soilMoisture[5];    //Scaled value of volumetric water content in soil (percent)
 float humidity = 0;        //Relative humidity (%)
 float airTemp = 0;         //Air temp (degrees F)
 float sunlight = 0; 	   //Sunlight illumination (lux)
@@ -95,33 +95,41 @@ void loop()
   delay(20);
   
   //Volumetric Water Content is a piecewise function of the voltage from the sensor
-  if (soilMoistureRaw <= 1.1)
+  for (int i = 0; i < 5; i++)
   {
-    soilMoisture = (10 * soilMoistureRaw) - 1;
-  }
-  else if (soilMoistureRaw > 1.1 && soilMoistureRaw <= 1.3)
-  {
-    soilMoisture = (25 * soilMoistureRaw) - 17.5;
-  }
-  else if (soilMoistureRaw > 1.3 && soilMoistureRaw <= 1.82)
-  {
-    soilMoisture = (48.08 * soilMoistureRaw) - 47.5;
-  }
-  else if (soilMoistureRaw > 1.82 && soilMoistureRaw < 2.2)
-  {
-    soilMoisture = (26.32 * soilMoistureRaw) - 7.89;
-  }
-  else {
-    soilMoisture = (62.5 * soilMoistureRaw) - 87.5;
-  }
+	soilMoistureRaw = analogRead(soilMoisturePin) * (3.8874 / 1024);
+	delay(20);
   
-  if (soilMoisture < 0)
-  {
-	soilMoisture = 0;
-  }
-  else if (soilMoisture > 100)
-  {
-	soilMoisture = 100;
+	//Volumetric Water Content is a piecewise function of the voltage from the sensor
+	if (soilMoistureRaw <= 1.1)
+	{
+		soilMoisture = (10 * soilMoistureRaw) - 1;
+	}
+	else if (soilMoistureRaw > 1.1 && soilMoistureRaw <= 1.3)
+	{
+		soilMoisture = (25 * soilMoistureRaw) - 17.5;
+	}
+	else if (soilMoistureRaw > 1.3 && soilMoistureRaw <= 1.82)
+	{
+		soilMoisture = (48.08 * soilMoistureRaw) - 47.5;
+	}
+	else if (soilMoistureRaw > 1.82 && soilMoistureRaw < 2.2)
+	{
+    soilMoisture = (26.32 * soilMoistureRaw) - 7.89;
+	}
+	else {
+		soilMoisture = (62.5 * soilMoistureRaw) - 87.5;
+	}
+
+	if (soilMoisture < 0)
+	{
+		soilMoisture = 0;
+	}
+	else if (soilMoisture > 100)
+	{
+		soilMoisture = 100;
+	}
+	delay(20);
   }
 
   //Collect humidity
@@ -140,7 +148,13 @@ void loop()
 #if ECHO_TO_SERIAL
   Serial.print(airTemp);
   Serial.print(",");
-  Serial.print(soilMoisture);
+  Serial.print(soilMoisture[0]);
+  Serial.print(",");
+  Serial.print(soilMoisture[1]);
+  Serial.print(",");
+  Serial.print(soilMoisture[2]);
+  Serial.print(",");
+  Serial.print(soilMoisture[3]);
   Serial.print(",");
   Serial.print(humidity);
   Serial.print(",");
@@ -151,7 +165,73 @@ void loop()
 	postData = "plantId=30&temperature=";
 	postData.concat(airTemp);
 	postData.concat("&moisture=");
-	postData.concat(soilMoisture);
+	postData.concat(soilMoisture[0]);
+	postData.concat("&humidity=");
+	postData.concat(humidity);
+	postData.concat("&sunlight=");
+	postData.concat(sunlight);
+    client.println("POST /setPlantTraits.php HTTP/1.1");
+    client.println("Host: 71142021.000webhostapp.com");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.print(postData);
+	delay(1000);
+  }
+  if (client.connected()) {
+    client.stop();
+  }
+  delay(100);
+  if (client.connect(server, 80)) {
+	postData = "plantId=31&temperature=";
+	postData.concat(airTemp);
+	postData.concat("&moisture=");
+	postData.concat(soilMoisture[1]);
+	postData.concat("&humidity=");
+	postData.concat(humidity);
+	postData.concat("&sunlight=");
+	postData.concat(sunlight);
+    client.println("POST /setPlantTraits.php HTTP/1.1");
+    client.println("Host: 71142021.000webhostapp.com");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.print(postData);
+	delay(1000);
+  }
+  if (client.connected()) {
+    client.stop();
+  }
+  delay(100);
+  if (client.connect(server, 80)) {
+	postData = "plantId=33&temperature=";
+	postData.concat(airTemp);
+	postData.concat("&moisture=");
+	postData.concat(soilMoisture[2]);
+	postData.concat("&humidity=");
+	postData.concat(humidity);
+	postData.concat("&sunlight=");
+	postData.concat(sunlight);
+    client.println("POST /setPlantTraits.php HTTP/1.1");
+    client.println("Host: 71142021.000webhostapp.com");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.print(postData);
+	delay(1000);
+  }
+  if (client.connected()) {
+    client.stop();
+  }
+  delay(100);
+  if (client.connect(server, 80)) {
+	postData = "plantId=34&temperature=";
+	postData.concat(airTemp);
+	postData.concat("&moisture=");
+	postData.concat(soilMoisture[3]);
 	postData.concat("&humidity=");
 	postData.concat(humidity);
 	postData.concat("&sunlight=");
